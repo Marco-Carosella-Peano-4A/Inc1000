429aebac887877c846ca2467732f101c3c15681a
Marco-Carosella-Peano-4A <56471111+Marco-Carosella-Peano-4A@users.noreply.github.com>
2020-02-08 09:33:56 +0100

    Add files via upload

---
+++ Inc1000.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestInc1000;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Marco
+ */
+class Inc1000 extends Thread{
+     static int count;
+
+    public synchronized void run() {
+        for (int i = 0; i < 1000; i++) {
+            count++;
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Inc1000.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+}
---
+++ TestInc1000.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestInc1000;
+
+/**
+ *
+ * @author Marco
+ */
+public class TestInc1000 {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) throws InterruptedException {
+        Inc1000 inc = new Inc1000();
+        Inc1000 inc2 = new Inc1000();
+        inc.start();      
+        inc2.start();
+        inc.join();
+        inc2.join();
+        System.out.println(Inc1000.count);
+    }
+    
+}
